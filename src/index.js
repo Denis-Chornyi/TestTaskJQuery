f84b44521ad21c578b67392043195cf1e24eb845
Denis Chornyi <chornyi.den@gmail.com>
2024-12-23 23:46:57 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
   function updateTexts() {
-    $(".header1__navigation-item-description").each(function () {
-      const newText =
-        window.innerWidth <= 990
-          ? $(this).data("small")
-          : $(this).data("large");
+    $('.header1__navigation-item-description').each(function () {
+      const newText = window.innerWidth <= 990 ? $(this).data('small') : $(this).data('large');
       $(this).text(newText);
     });
   }
@@ -18,22 +15,22 @@ $(document).ready(function () {
   let timer = 90;
   setInterval(() => {
     timer--;
-    $("#timer").text(timer);
+    $('#timer').text(timer);
 
     if (timer === 0) {
       timer = 90;
 
-      $("#header1").toggleClass("visible hidden");
-      $("#header2").toggleClass("visible hidden");
+      $('#header1').toggleClass('visible hidden');
+      $('#header2').toggleClass('visible hidden');
 
-      $("#footer1").toggleClass("visible hidden");
-      $("#footer2").toggleClass("visible hidden");
+      $('#footer1').toggleClass('visible hidden');
+      $('#footer2').toggleClass('visible hidden');
     }
   }, 1000);
 });
 
 $(document).ready(function () {
-  $(".scroll-to-top").on("click", function () {
-    $("html, body").animate({ scrollTop: 0 }, 800);
+  $('.scroll-to-top').on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
   });
 });
